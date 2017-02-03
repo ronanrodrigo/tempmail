bebb14512ee1a4147203eb7ca60a59de91c7a75e
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-02-03 01:03:44 -0200

    Create navigation controller and add it at root view

--- TempMail/Base/AppDelegate.swift
+++ TempMail/Base/AppDelegate.swift
@@ -7,6 +7,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+
+        let mailBoxNavigationCotroller = UINavigationController(rootViewController: MailboxViewController())
+        window?.rootViewController = mailBoxNavigationCotroller
+
+        window?.makeKeyAndVisible()
         return true
     }
 
